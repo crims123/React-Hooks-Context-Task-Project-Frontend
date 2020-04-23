a230b06f0421549687463983288e18e60eb35369
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-04-23 07:31:38 -0500

    Added login component

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,9 +1,58 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import useInput from "../../hooks/useInput";
 
 function Login() {
+  const [value, handleChange] = useInput({ email: "", password: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(value);
+  };
+
   return (
-    <div>
-      <h2>hi from Login</h2>
+    <div className="form-usuario">
+      {/* { alerta ? ( <div className={`alerta ${alerta.categoria}`}> {alerta.msg} </div> )  : null } */}
+
+      <div className="contenedor-form sombra-dark">
+        <h1>Log in</h1>
+
+        <form onSubmit={handleSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Add Email"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Add Password"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Log in"
+            />
+          </div>
+        </form>
+
+        <Link to={"/register"} className="enlace-cuenta">
+          Register
+        </Link>
+      </div>
     </div>
   );
 }
---
+++ src/hooks/useInput.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function useInput(initialValues) {
+  const [values, setValues] = useState(initialValues);
+
+  function handleChange(e) {
+    e.preventDefault();
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  return [values, handleChange];
+}
+
+export default useInput;
